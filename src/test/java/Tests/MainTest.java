6e10dd079004d34ac988895a2f0af94765efdf0a
Cengizhan Uzuner <77883086+cengisan@users.noreply.github.com>
2022-03-31 20:09:54 +0300

    Update MainTest.java
    Some Xpath types are changed. I used relative Xpath in somewhere. So, Test will never affect changes on the site.

--- src/test/java/Tests/MainTest.java
+++ src/test/java/Tests/MainTest.java
@@ -1,14 +1,16 @@
 package Tests;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
 import java.util.concurrent.TimeUnit;
 
-
-public class MainTest extends UserDataJsonParser  {
+public class MainTest extends UserDataJsonParser {
 
     protected WebDriver driver;
     public static String baseUrl = "https://www.amazon.com.tr/";
@@ -19,7 +21,8 @@ public class MainTest extends UserDataJsonParser  {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
-    @Test(priority=1)
+
+    @Test(priority = 1)
     public void login() throws InterruptedException {
 
         driver.get(baseUrl);
@@ -30,38 +33,60 @@ public class MainTest extends UserDataJsonParser  {
         TimeUnit.SECONDS.sleep(2);
         driver.findElement(By.id("ap_password")).sendKeys(user.getPassword());
         driver.findElement(By.id("signInSubmit")).click();
-
-
     }
 
-    @Test(priority=2)
-    public void searchItem() throws InterruptedException {
+    @Test(priority = 2)
+    public void searchProductAndAddToCart() throws InterruptedException {
 
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("İphone pro max 13");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Iphone pro max 13");
         driver.findElement(By.id("nav-search-submit-button")).click();
         driver.findElement(By.xpath("//*[@id=\"a-autoid-0-announce\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"s-result-sort-select_2\"]")).click();
         TimeUnit.SECONDS.sleep(2);
-        driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[10]/div/div/div/div/div[1]/span/a/div/img")).click();
+        driver.findElement(By.xpath("//div[contains(@data-index,10)]")).click();
         TimeUnit.SECONDS.sleep(2);
-        driver.findElement(By.xpath("//*[@id=\"simpleBundleV2_feature_div\"]/div/div[1]/div/div/span[1]/div/label/input")).click();
-        TimeUnit.SECONDS.sleep(2);
+        WebElement element = driver.findElement(By.id("desktop_qualifiedBuyBox"));
+        boolean value = element.isDisplayed();
+
+        if (value == true) {
+            driver.findElement(By.xpath("//*[@id=\"simpleBundleV2_feature_div\"]/div/div[1]/div/div/span[1]/div/label/input")).click();
+            addToCartTwoOrder();
+        }
+        else {
+            addToCartSingleOrder();
+        }
     }
+    
+        void addToCartTwoOrder() throws InterruptedException {
+
+            //add your order
+            driver.findElement(By.id("add-to-cart-button")).click();
+            TimeUnit.SECONDS.sleep(2);
+
+            driver.findElement(By.xpath("//*[@id=\"attach-sidesheet-view-cart-button\"]/span/input")).click();
+            TimeUnit.SECONDS.sleep(2);
+
+            //delete an order
+            driver.findElement(By.className("a-color-link")).click();
+            TimeUnit.SECONDS.sleep(2);
+        }
+    
+        void addToCartSingleOrder() throws InterruptedException {
+            //add your order
+            driver.findElement(By.id("add-to-cart-button")).click();
+            TimeUnit.SECONDS.sleep(2);
+
+            driver.findElement(By.xpath("//*[@id=\"sw-gtc\"]/span/a")).click();
+            TimeUnit.SECONDS.sleep(2);
+
+            //delete an order
+            driver.findElement(By.className("a-color-link")).click();
+            TimeUnit.SECONDS.sleep(2);
+
+        }
 
     @Test(priority = 3)
-    public void orderItem() throws InterruptedException {
-
-        //add your order
-        driver.findElement(By.id("add-to-cart-button")).click();
-        TimeUnit.SECONDS.sleep(2);
-
-        driver.findElement(By.xpath("//*[@id=\"attach-sidesheet-view-cart-button\"]/span/input")).click();
-        TimeUnit.SECONDS.sleep(2);
-
-        //delete an order
-        driver.findElement(By.className("a-color-link")).click();
-        TimeUnit.SECONDS.sleep(2);
-
+    public void addToCartAnotherOrder() throws InterruptedException {
         //add another order
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("xiaomi headphones");
         TimeUnit.SECONDS.sleep(2);
@@ -69,7 +94,7 @@ public class MainTest extends UserDataJsonParser  {
         driver.findElement(By.id("nav-search-submit-button")).click();
         TimeUnit.SECONDS.sleep(1);
 
-        driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div[3]/div/div/div/div/div[1]/span/a/div/img")).click();
+        driver.findElement(By.xpath("//img[contains(@data-image-index,2)]")).click();
         TimeUnit.SECONDS.sleep(2);
 
         driver.findElement(By.id("add-to-cart-button")).click();
@@ -79,16 +104,17 @@ public class MainTest extends UserDataJsonParser  {
         TimeUnit.SECONDS.sleep(2);
 
     }
+
     @Test(priority = 4)
     public void deleteOrders() throws InterruptedException {
 
         driver.findElement(By.className("a-color-link")).click();
         TimeUnit.SECONDS.sleep(2);
-
         driver.findElement(By.className("a-color-link")).click();
         TimeUnit.SECONDS.sleep(2);
 
     }
+
     @Test(priority = 5)
     public void logout() throws InterruptedException {
 
@@ -98,10 +124,8 @@ public class MainTest extends UserDataJsonParser  {
         driver.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[16]/a")).click();
         TimeUnit.SECONDS.sleep(2);
     }
-
     @AfterTest
     public void tearDown() {
         driver.quit();
     }
-
 }
