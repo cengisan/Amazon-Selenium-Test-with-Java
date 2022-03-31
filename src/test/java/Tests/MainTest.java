ba1be8102293ccad3a66a2ef4d97cbe405558567
Cengizhan Uzuner <77883086+cengisan@users.noreply.github.com>
2022-03-31 22:58:19 +0300

    Update MainTest.java

--- src/test/java/Tests/MainTest.java
+++ src/test/java/Tests/MainTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
