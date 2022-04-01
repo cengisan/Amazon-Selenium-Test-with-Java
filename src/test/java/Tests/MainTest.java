a1e50b1f7788b5eac8291b37ed09b9f4afe1e756
Cengizhan Uzuner <77883086+cengisan@users.noreply.github.com>
2022-04-02 00:18:48 +0300

    Update MainTest.java
    I defined a elementFinder. In this way, codes became more readable. Also, I set timeouts with visibilityOfElementLocated. Thus, test running will wait web elements become visible.

--- src/test/java/Tests/MainTest.java
+++ src/test/java/Tests/MainTest.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class MainTest extends UserDataJsonParser {
 
+    public WebDriverWait wait;
     protected WebDriver driver;
     public static String baseUrl = "https://www.amazon.com.tr/";
 
@@ -20,96 +23,77 @@ public class MainTest extends UserDataJsonParser {
         System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+    }
+
+    public WebElement elementFinder(By selector) {
+        //wait until the web element become visible.
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(selector));
     }
 
     @Test(priority = 1)
-    public void login() throws InterruptedException {
+    public void login() {
 
         driver.get(baseUrl);
-        driver.findElement(By.id("nav-link-accountList")).click();
-        TimeUnit.SECONDS.sleep(2);
-        driver.findElement(By.id("ap_email")).sendKeys(user.getEmail());
-        driver.findElement(By.id("continue")).click();
-        TimeUnit.SECONDS.sleep(2);
-        driver.findElement(By.id("ap_password")).sendKeys(user.getPassword());
-        driver.findElement(By.id("signInSubmit")).click();
+        elementFinder(By.id("nav-link-accountList")).click();
+        elementFinder(By.id("ap_email")).sendKeys(user.getEmail());
+        elementFinder(By.id("continue")).click();
+        elementFinder(By.id("ap_password")).sendKeys(user.getPassword());
+        elementFinder(By.id("signInSubmit")).click();
     }
 
     @Test(priority = 2)
     public void searchProductAndAddToCart() throws InterruptedException {
 
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Iphone pro max 13");
-        driver.findElement(By.id("nav-search-submit-button")).click();
-        driver.findElement(By.xpath("//*[@id=\"a-autoid-0-announce\"]")).click();
-        driver.findElement(By.xpath("//*[@id=\"s-result-sort-select_2\"]")).click();
-        TimeUnit.SECONDS.sleep(2);
-
+        elementFinder(By.id("twotabsearchtextbox")).sendKeys("Iphone pro max 13");
+        elementFinder(By.id("nav-search-submit-button")).click();
+        elementFinder(By.xpath("//*[@id=\"a-autoid-0-announce\"]")).click();
+        elementFinder(By.xpath("//*[@id=\"s-result-sort-select_2\"]")).click();
         //Set order according to data-index
-        driver.findElement(By.xpath("//div[contains(@data-index,12)]")).click();
-        TimeUnit.SECONDS.sleep(2);
-
+        elementFinder(By.xpath("//div[contains(@data-index,12)]")).click();
         WebElement a = driver.findElement(By.id("simpleBundleV2_feature_div"));
         boolean value = a.isEnabled();
 
         if (value) {
-            driver.findElement(By.xpath("//*[@id=\"simpleBundleV2_feature_div\"]/div/div[1]/div/div/span[1]/div/label/input")).click();
+            elementFinder(By.xpath("//*[@id=\"simpleBundleV2_feature_div\"]/div/div[1]/div/div/span[1]/div/label/input")).click();
             //add your order
-            driver.findElement(By.id("add-to-cart-button")).click();
-            TimeUnit.SECONDS.sleep(2);
-            driver.findElement(By.xpath("//*[@id=\"attach-sidesheet-view-cart-button\"]/span/input")).click();
-            TimeUnit.SECONDS.sleep(2);
+            elementFinder(By.id("add-to-cart-button")).click();
+            elementFinder(By.xpath("//*[@id=\"attach-sidesheet-view-cart-button\"]/span/input")).click();
+
         } else {
             //add your order
-            driver.findElement(By.id("add-to-cart-button")).click();
-            TimeUnit.SECONDS.sleep(2);
-
-            driver.findElement(By.xpath("//*[@id=\"sw-gtc\"]/span/a")).click();
-            TimeUnit.SECONDS.sleep(2);
+            elementFinder(By.id("add-to-cart-button")).click();
+            elementFinder(By.xpath("//*[@id=\"sw-gtc\"]/span/a")).click();
         }
 
         //delete an order
-        driver.findElement(By.className("a-color-link")).click();
-        TimeUnit.SECONDS.sleep(2);
+        elementFinder(By.className("a-color-link")).click();
     }
 
     @Test(priority = 3)
     public void addToCartAnotherOrder() throws InterruptedException {
         //add another order
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("xiaomi headphones");
-        TimeUnit.SECONDS.sleep(2);
-
-        driver.findElement(By.id("nav-search-submit-button")).click();
-        TimeUnit.SECONDS.sleep(1);
-
-        driver.findElement(By.xpath("//img[contains(@data-image-index,2)]")).click();
-        TimeUnit.SECONDS.sleep(2);
-
-        driver.findElement(By.id("add-to-cart-button")).click();
-        TimeUnit.SECONDS.sleep(2);
-
-        driver.findElement(By.xpath("//*[@id=\"sw-gtc\"]/span/a")).click();
-        TimeUnit.SECONDS.sleep(2);
+        elementFinder(By.id("twotabsearchtextbox")).sendKeys("xiaomi headphones");
+        elementFinder(By.id("nav-search-submit-button")).click();
+        elementFinder(By.xpath("//img[contains(@data-image-index,2)]")).click();
+        elementFinder(By.id("add-to-cart-button")).click();
+        elementFinder(By.xpath("//*[@id=\"sw-gtc\"]/span/a")).click();
 
     }
 
     @Test(priority = 4)
     public void deleteOrders() throws InterruptedException {
 
-        driver.findElement(By.className("a-color-link")).click();
-        TimeUnit.SECONDS.sleep(2);
-        driver.findElement(By.className("a-color-link")).click();
-        TimeUnit.SECONDS.sleep(2);
+        elementFinder(By.className("a-color-link")).click();
+        elementFinder(By.className("a-color-link")).click();
 
     }
 
     @Test(priority = 5)
     public void logout() throws InterruptedException {
 
-        driver.findElement(By.id("nav-hamburger-menu")).click();
-        TimeUnit.SECONDS.sleep(2);
-
-        driver.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[16]/a")).click();
-        TimeUnit.SECONDS.sleep(2);
+        elementFinder(By.id("nav-hamburger-menu")).click();
+        elementFinder(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[16]/a")).click();
     }
 
     @AfterTest
