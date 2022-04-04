0b39f1b0b59b4a6fef33d191002fd366358f936f
Cengizhan Uzuner <77883086+cengisan@users.noreply.github.com>
2022-04-04 10:18:22 +0300

    Create UserInfoGetSet.java

---
+++ src/driver/test/database/UserInfoGetSet.java
@@ -0,0 +1,24 @@
+package database;
+
+public class UserInfoGetSet {
+
+    private static String email;
+    private static String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
